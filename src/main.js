6762a49053fc93908ededca9ab722b80f406d5b2
oswvld <daniel.demonteverde@gmail.com>
2021-03-16 16:00:37 +0100

    added vue scrollto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
@@ -7,10 +8,15 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import './ml'
 
+import VScrollLock from 'v-scroll-lock'
+Vue.use(VScrollLock)
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(VueScrollTo)
 
 Vue.config.productionTip = false
 
