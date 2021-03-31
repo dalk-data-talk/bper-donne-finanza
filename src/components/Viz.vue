0cf27228b99e6501c2ae062ff58a2ee8f1837d50
oswvld <daniel.demonteverde@gmail.com>
2021-04-01 01:35:04 +0200

    edited viz component

--- src/components/Viz.vue
+++ src/components/Viz.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="viz-container">
-    viz
+    <b-container>
+      <b-row>
+        <b-col>
+          <div class="title" v-html="content.title"></div>
+          <div class="description" v-html="content.description"></div>
+          <div class="img"></div>
+          <div class="source" v-html="content.source"></div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -20,6 +29,23 @@ export default {
 @import "@/assets/scss/bper-fonts.scss";
 @import "@/assets/scss/colors.scss";
 .viz-container{
-  
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: $bper-verde-scuro;
+  margin-bottom: 40px;
+  .title{
+    text-transform: uppercase;
+    padding-top: 40px;
+    padding-bottom: 12px;
+    @include viz-title;
+  }
+  .description{
+    padding-right: 100px;
+    @include footer-text-element;
+  }
+  .source{
+    @include viz-source;
+  }
 }
 </style>
