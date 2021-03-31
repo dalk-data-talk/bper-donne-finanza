85c42efcbb598a4de77081517df6f58be50d43bb
oswvld <daniel.demonteverde@gmail.com>
2021-03-31 12:48:27 +0200

    added viz

---
+++ src/components/Viz.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="viz-container">
+    viz
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Viz',
+  props: ["content"],
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
+<style lang="scss" scoped>
+@import "@/assets/scss/bper-fonts.scss";
+@import "@/assets/scss/colors.scss";
+.viz-container{
+  
+}
+</style>
