dc27354309c3fcd3f3c996507c17fe5c4ca0db5f
oswvld <daniel.demonteverde@gmail.com>
2021-03-31 12:47:38 +0200

    added highlight

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="highlight-container">
-    <div class="text" v-text="content.text"></div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <div class="text" v-text="content.text"></div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -17,9 +23,10 @@ export default {
 
 <!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
 <style lang="scss" scoped>
-@import "@/assets/scss/fonts.scss";
+@import "@/assets/scss/bper-fonts.scss";
 @import "@/assets/scss/colors.scss";
 .highlight-container{
+  padding-bottom: 32px;
   .text{
     @include highlight;
   }
