a33147183da1116e76124f3ed4e0eb8579bf4702
oswvld <daniel.demonteverde@gmail.com>
2021-04-07 17:52:31 +0200

    added switch mobile - desktop

--- src/components/Viz.vue
+++ src/components/Viz.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="viz-container">
-    <b-container>
-      <b-row>
-        <b-col>
-          <div class="title" v-html="content.title"></div>
-          <div class="description" v-html="content.description"></div>
-          <img class="img" :src="'./images/'+content.img"/>
-          <div class="source" v-html="content.source"></div>
-        </b-col>
-      </b-row>
-    </b-container>
+  <div>
+    <!-- mobile / tablet -->
+    <div v-if="$mq != 'desktop'" class="viz-container">
+      <b-container>
+        <b-row>
+          <b-col>
+            <div class="title" v-html="content.title"></div>
+            <div class="description" v-html="content.description"></div>
+            <img class="img" :src="'./images/'+content.img"/>
+            <div class="source" v-html="content.source"></div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <!-- desktop -->
+    <div v-else class="viz-container-desktop">
+      <b-container>
+        <b-row>
+          <b-col>
+            <div class="title" v-html="content.title"></div>
+            <div class="description" v-html="content.description"></div>
+            <img class="img" :src="'./images/'+content.img"/>
+            <div class="source" v-html="content.source"></div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -29,6 +45,31 @@ export default {
 @import "@/assets/scss/bper-fonts.scss";
 @import "@/assets/scss/colors.scss";
 .viz-container{
+  position: relative;
+  width: 100%;
+  background-color: $bper-verde-scuro;
+  margin-bottom: 40px;
+  .title{
+    text-transform: uppercase;
+    padding-top: 40px;
+    padding-bottom: 12px;
+    @include viz-title;
+  }
+  .description{
+    padding-right: 100px;
+    @include footer-text-element;
+    padding-bottom: 24px;
+  }
+  .img{
+    width: 100%;
+    margin-bottom: 24px;
+  }
+  .source{
+    @include viz-source;
+    padding-bottom: 20px;
+  }
+}
+.viz-container-desktop{
   position: relative;
   width: 100%;
   background-color: $bper-verde-scuro;
