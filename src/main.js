28fa82442a7ef554b11a80e407ad0b61a1fe604d
oswvld <daniel.demonteverde@gmail.com>
2021-04-07 17:51:55 +0200

    added vue mq

--- src/main.js
+++ src/main.js
@@ -7,12 +7,22 @@ import VScrollLock from 'v-scroll-lock'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './ml'
+import VueMq from 'vue-mq'
 
 Vue.use(VScrollLock)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(VueScrollTo)
 Vue.use(VueCarousel);
+
+Vue.use(VueMq, {
+  breakpoints: { // default breakpoints - customize this
+    mobile: 450,
+    tablet: 1250,
+    desktop: Infinity,
+  },
+  defaultBreakpoint: 'mobile' // customize this for SSR
+})
 
 Vue.config.productionTip = false
 
