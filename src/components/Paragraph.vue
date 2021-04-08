ca43f46e4099e4cd419508adcd80dc1013642035
oswvld <daniel.demonteverde@gmail.com>
2021-04-08 14:51:40 +0200

    updated formattazione

--- src/components/Paragraph.vue
+++ src/components/Paragraph.vue
@@ -2,22 +2,25 @@
   <div class="paragraph-container">
     <b-container>
       <b-row v-if="!content.note">
-        <b-col xl="8" class="text-container">
+        <b-col lg="8" class="text-container">
           <div class="text" v-html="content.text"></div>
         </b-col>
       </b-row>
       <b-row v-else>
-        <b-col class="note-container">
-          <div class="note">
-            <div>{{content.note.title}}</div>
-            <div v-for="(row,r) in content.note.rows" :key="r">
-              <div>{{row.number}}</div>
-              <div>{{row.description}}</div>
-            </div>
-            <div>{{content.note.source}}</div>
-          </div>
+        <b-col sm="12" lg="4" class="note-container">
+          <NumericHighlight :content="content.note"/>
         </b-col>
-        <b-col class="text-container">
+        <!-- <b-col sm="12" lg="3" class="note-container">
+          <div class="note">
+            <div class="title">{{content.note.title}}</div>
+            <div class="row-data" v-for="(row,r) in content.note.rows" :key="r">
+              <div class="row-number">{{row.number}}</div>
+              <div class="row-description">{{row.description}}</div>
+            </div>
+            <div class="source">{{content.note.source}}</div>
+          </div>
+        </b-col> -->
+        <b-col lg="6" class="text-container">
           <div class="text" v-html="content.text"></div>
         </b-col>
       </b-row>
@@ -26,12 +29,16 @@
 </template>
 
 <script>
+import NumericHighlight from '../components/NumericHighlight.vue'
 export default {
   name: 'Paragraph',
   props: ["content"],
   mounted() {
   },
   methods: {
+  },
+  components: {
+    NumericHighlight
   }
 };
 </script>
@@ -47,6 +54,12 @@ export default {
     justify-content: center;
     .text{
       @include paragraph;
+      .bold{
+        @include paragraph-bold;
+      }
+    }
+    .source{
+      @include numeric-highlight-source;
     }
   }
 }
@@ -54,18 +67,64 @@ export default {
 @media only screen 
   and (min-device-width: 450px) 
   and (max-device-width: 1250px) {
-
 }
-// -- media query desktop
+// -- media query tablet (landscape) + desktop
 @media only screen 
-  and (min-device-width: 1250px)
-  and (orientation: landscape)  {
+  and (min-device-width: 768px) 
+  and (orientation: landscape) {
     .paragraph-container{
       .row{
+        .numeric-highlight-container{
+          .box{
+            .title{
+            }
+            .source{
+              text-align: left;
+            }
+          }
+        }
         .text{
           @include paragraph--desktop;
         }
       }
     }
 }
+</style>
+<style lang="scss">
+@import "@/assets/scss/bper-fonts.scss";
+@import "@/assets/scss/colors.scss";
+.paragraph-container{
+  .row{
+    .text{
+      .bold{
+        @include paragraph-bold;
+      }
+      .italic{
+            @include paragraph-italic;
+          }
+      .link{
+        @include paragraph-link;
+      }
+    }
+  }
+}
+@media only screen 
+  and (min-device-width: 768px) 
+  and (orientation: landscape) {
+    .paragraph-container{
+      .row{
+        .text{
+          .bold{
+            @include paragraph-bold--desktop;
+          }
+          .italic{
+            @include paragraph-italic--desktop;
+          }
+          .link{
+            @include paragraph-link--desktop;
+          }
+        }
+      }
+    }
+  }
 </style>
